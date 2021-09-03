381c95059e5b8286f1019a51ca20438762d5f50b
mugdhamalpe <84629815+Mugdhaaa04@users.noreply.github.com>
2021-09-03 21:53:41 +0530

    Task 2

--- printnumbers/urls.py
+++ printnumbers/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index'),
-    path('enternumber', views.enternumber, name='enternumber'),
+    path('', views.index, name='enternumber'),
     # path(r'^one/$', RedirectView.as_view(url='http://127.0.0.1:8000')),
 ]
--- printnumbers/views.py
+++ printnumbers/views.py
@@ -4,12 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'input.html')
-
-def enternumber(request):
-    number = int(request.GET["number"])
-    if(number > 0 and number < 500):
-        result = [n for n in range(1, number+1)]
-        return render(request, 'number.html', { "resnumber": result , "link":"http://127.0.0.1:8000"})
+    if request.method == "POST" :
+        number = request.POST.get("number")
+        if(int(number) > 0 and int(number) < 500):
+            result = [n for n in range(1, int(number)+1)]
+            return render(request, 'number.html', { "resnumber": result })
+        else:
+            return render(request, 'invalid.html', {})
     else:
-        return render(request, 'invalid.html', {"link":"http://127.0.0.1:8000"})
+        return render(request, 'input.html', {})
+
